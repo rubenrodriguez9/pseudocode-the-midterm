4ae13673094dbcdc9c825badff932fcb3d377643
Colin Jaffe <balloonasaurus@gmail.com>
2020-07-05 00:56:17 -0400

    Adds first two problems.

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
+const isValidPassword = function(password) {
+  if (password.length < 12) {
+    return false;
+  }
+
+  for (let i = 0; i < password.length; i++) {
+    if (password[i] === ' ') {
+      return false;
+    }
+  }
+
+  return true;
+
+  // using includes instead of all above code
+  // return !password.includes(' ') && password.length >= 12;
+}
+
+const capitalizeSentences = function(paragraph) {
+  let result = '';
+
+  for (let i = 0; i < paragraph.length; i++) {
+    if (paragraph[i - 2] === '.' || i === 0) {
+      result = result + paragraph[i].toUpperCase();
+    } else {
+      result = result + paragraph[i].toLowerCase();
+    }
+  }
+
+  return result;
+}
 
 
+if (typeof capitalizeSentences === 'undefined') {
+  capitalizeSentences = undefined;
+}
 
-
-
-
-
-if (typeof makeRunOn === 'undefined') {
-  makeRunOn = undefined;
+if (typeof isValidPassword === 'undefined') {
+  isValidPassword = undefined;
 }
 
 
 module.exports = {
-  makeRunOn,
+  capitalizeSentences,
+  isValidPassword,
 }
--- main.test.js
+++ main.test.js
@@ -1,26 +1,52 @@
 const {
-  makeRunOn,
+  capitalizeSentences,
+  isValidPassword,
 } = require('./main.js')
 
 
-describe('makeRunOn', () => {
-  it(`replaces periods with commas to make the given string a run-on sentence`, () => {
-    const original1 = 'Run-ons are the best. Why would you pause. When you can just. Keep. GOING';
-    const runOn1 = 'Run-ons are the best, Why would you pause, When you can just, Keep, GOING';
-    const original2 = 'See spot run. See spot run on and on and on and';
-    const runOn2 = 'See spot run, See spot run on and on and on and';
-    
-    expect(makeRunOn(original1)).toBe(runOn1)
-    expect(makeRunOn(original2)).toBe(runOn2)
+describe('capitalizeSentences', () => {
+  it(`capitalizes every sentence after the first in the given paragraph`, () => {
+    const original1 = 'Binge-watching is the best. why would you pause. when you can just. keep. going.';
+    const capitalized1 = 'Binge-watching is the best. Why would you pause. When you can just. Keep. Going.';
+    const original2 = 'Example sentences are hard to come up with. you just try it. you will, huh. go on, try it then.';
+    const capitalized2= 'Example sentences are hard to come up with. You just try it. You will, huh. Go on, try it then.';
+
+    expect(capitalizeSentences(original1)).toBe(capitalized1)
+    expect(capitalizeSentences(original2)).toBe(capitalized2)
   })
 
-  it(`keeps the string as a sentence--does NOT remove the last period.`, () => {
-    const original1 = `And once the storm is over you won't remember how you made it through, how you managed to survive. You won't even be sure, in fact, whether the storm is really over. But one thing is certain. When you come out of the storm you won't be the same person who walked in. That's what this storm's all about.`;
-    const runOn1 = `And once the storm is over you won't remember how you made it through, how you managed to survive, You won't even be sure, in fact, whether the storm is really over, But one thing is certain, When you come out of the storm you won't be the same person who walked in, That's what this storm's all about.`;
+  it(`capitalizes the first sentence as well`, () => {
+    const original1 = `and once the storm is over you won't remember how you made it through, how you managed to survive. you won't even be sure, in fact, whether the storm is really over. but one thing is certain. when you come out of the storm you won't be the same person who walked in. that's what this storm's all about.`;
+    const capitalized1 = `And once the storm is over you won't remember how you made it through, how you managed to survive. You won't even be sure, in fact, whether the storm is really over. But one thing is certain. When you come out of the storm you won't be the same person who walked in. That's what this storm's all about.`;
+    const original2 = 'the ministry of truth, which concerned itself with news, entertainment, education and the fine arts. the ministry of peace, which concerned itself with war. the ministry of love, which maintained law and order. and the ministry of plenty, which was responsible for economic affairs. their names, in newspeak: minitrue, minipax, miniluv and miniplenty.'
+    const capitalized2 = 'The ministry of truth, which concerned itself with news, entertainment, education and the fine arts. The ministry of peace, which concerned itself with war. The ministry of love, which maintained law and order. And the ministry of plenty, which was responsible for economic affairs. Their names, in newspeak: minitrue, minipax, miniluv and miniplenty.'
+    expect(capitalizeSentences(original1)).toBe(capitalized1)
+    expect(capitalizeSentences(original2)).toBe(capitalized2)
+  })
 
-    const original2 = 'The Ministry of Truth, which concerned itself with news, entertainment, education and the fine arts. The Ministry of Peace, which concerned itself with war. The Ministry of Love, which maintained law and order. And the Ministry of Plenty, which was responsible for economic affairs. Their names, in Newspeak: Minitrue, Minipax, Miniluv and Miniplenty.'
-    const runOn2 = 'The Ministry of Truth, which concerned itself with news, entertainment, education and the fine arts, The Ministry of Peace, which concerned itself with war, The Ministry of Love, which maintained law and order, And the Ministry of Plenty, which was responsible for economic affairs, Their names, in Newspeak: Minitrue, Minipax, Miniluv and Miniplenty.'
-    expect(makeRunOn(original1)).toBe(runOn1)
-    expect(makeRunOn(original2)).toBe(runOn2)
+  it(`lowercases all other characters`, () => {
+    const original1 = 'In a way fighting was just like using Magic. You said the words, and they alterEd the universe. By merely speaking you could create damage and pain, cause tEars to fall, drive people Away, make yourself feel better, make your life worsE.'
+    const capitalized1 = 'In a way fighting was just like using magic. You said the words, and they altered the universe. By merely speaking you could create damage and pain, cause tears to fall, drive people away, make yourself feel better, make your life worse.'
+    const original2 = 'A magIcian is strong because he feels pain. He Feels the difference beTween what the world is and what he would make of it.'
+    const capitalized2 = 'A magician is strong because he feels pain. He feels the difference between what the world is and what he would make of it.'
+    expect(capitalizeSentences(original1)).toBe(capitalized1)
+    expect(capitalizeSentences(original2)).toBe(capitalized2)
+  })
+})
+
+describe('isValidPassword', () => {
+  it(`returns false if there is a space in the given string`, () => {
+    expect(isValidPassword('hey no this is not how it should be')).toBe(false);
+    expect(isValidPassword('bad password')).toBe(false);
+  })
+
+  it(`returns false if the given string's length is less than 12 characters`, () => {
+    expect(isValidPassword('shorty')).toBe(false);
+    expect(isValidPassword('ritemoarpls')).toBe(false);
+  })
+
+  it(`returns true otherwise`, () => {
+    expect(isValidPassword('longpasswordwithnospaces')).toBe(true)
+    expect(isValidPassword('justlongenuf')).toBe(true)
   })
 })
